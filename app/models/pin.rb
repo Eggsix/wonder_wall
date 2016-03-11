39b2e053a5de7fa0b191e312da1d18bb8f9922ba
Preston <plphan206@yahoo.com>
2016-03-10 22:12:14 -0800

    image attr_accessor

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,5 @@
 class Pin < ActiveRecord::Base
+	attr_accessor :image_file_name
 	belongs_to :user
 	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 	validates_attachment :image, presence: true, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
